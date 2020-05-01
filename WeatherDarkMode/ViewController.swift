d0da467a15c12f0be630a4165315686d69b78a88
kingsleychuuu <kingsleychu0730@hotmail.com>
2020-05-01 16:56:54 -0400

    Initial setup

---
+++ WeatherDarkMode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherDarkMode
+//
+//  Created by admin on 2020-05-01.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
